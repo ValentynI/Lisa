afe537b2d01b1c5b84a212b8a649bdac416a15b6
ValentynI <ishchenkovalik@gmail.com>
2022-05-17 17:16:12 +0300

    commit2

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-#name = input("Введіть ім'я")
-#print("Вітаю", (name+" ")*5,"!")
+name = input("Введіть ім'я")
+print("Вітаю", (name+" ")*5,"!")
 
-#s = input("Введіть слово")
-#s1 = s[2:7]
-#s2 = s[4]+s[3]+s[5:7]+s[1]+s[7:]
+s = input("Введіть слово")
+s1 = s[2:7]
+s2 = s[4]+s[3]+s[5:7]+s[1]+s[7:]
 
-#money = int(input("стартовий капітал:"))
-#nPC = int(input("Введіть кількість компютерів:"))
-#mPC = int(input("Введіть вартість компютерів:"))
-#nNB = int(input("Введіть кількість ноутбуків:"))
-#mNB = int(input("Введіть вартість ноутбуків:"))
-#nMouse = int(input("Введіть кількість мишок:"))
-#mMouse = int(input("Введіть вартість мишок:"))
-#nKey = int(input("Введіть кількість клавіатур:"))
-#mKey = int(input("Введіть вартість клавіатур:"))
-#number = (nPC*mPC) + (nNB*mNB) + (nMouse*mMouse) + (nKey*mKey)
-#all_number = (money-number)
-#print("Залишок:", all_number)
+money = int(input("стартовий капітал:"))
+nPC = int(input("Введіть кількість компютерів:"))
+mPC = int(input("Введіть вартість компютерів:"))
+nNB = int(input("Введіть кількість ноутбуків:"))
+mNB = int(input("Введіть вартість ноутбуків:"))
+nMouse = int(input("Введіть кількість мишок:"))
+mMouse = int(input("Введіть вартість мишок:"))
+nKey = int(input("Введіть кількість клавіатур:"))
+mKey = int(input("Введіть вартість клавіатур:"))
+number = (nPC*mPC) + (nNB*mNB) + (nMouse*mMouse) + (nKey*mKey)
+all_number = (money-number)
+print("Залишок:", all_number)
 
-#a = int(input("перша основа:"))
-#b = int(input("друга основа:"))
-#c = int(input("перша бічна сторона:"))
-#d = int(input("друга бічна сторона:"))
-#h = int(input("висота:"))
-#number = (a + b) + (c + d)
-#s = ((a+b)//2)*h
-#print("Периметр:", number)
-#print("Площа:", s)
+a = int(input("перша основа:"))
+b = int(input("друга основа:"))
+c = int(input("перша бічна сторона:"))
+d = int(input("друга бічна сторона:"))
+h = int(input("висота:"))
+number = (a + b) + (c + d)
+s = ((a+b)//2)*h
+print("Периметр:", number)
+print("Площа:", s)
+
 
 
 
